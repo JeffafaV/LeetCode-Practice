9dc9d576c5ca1f043bda1888bf5a9adb05f63dc5
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-02-22 13:59:56 -0800

    Update climbing_stairs.cpp

--- climbing_stairs.cpp
+++ climbing_stairs.cpp
@@ -8,24 +8,24 @@ using namespace std;
 // space complexity: O(n) because we create a vector of size n
 int climbStairs(int n)
 {
-	// each entry is the number of steps needed for that index
+	// each entry is the number of ways you can climb up for that number of steps (index)
 	// the size is n+1 because the range is [0, n]
-	vector<int> steps(n+1);
+	vector<int> ways(n+1);
 	
 	// for 0 steps we are inserting a default value of 1
-	dp[0] = 1;
+	ways[0] = 1;
 	// for 1 step there is only one way to climb up
-	dp[1] = 1;
+	ways[1] = 1;
 	
 	// loop through the rest of the vector
-	for (int i = 2; i < steps.size(); i++)
+	for (int i = 2; i < ways.size(); i++)
 	{
-		// to get the number of steps for the current index 
+		// to get the number of ways for the current index 
 		// we have to add the values of the previous two values
-		steps[i] = steps[i-1] + steps[-2];
+		ways[i] = ways[i-1] + ways[i-2];
 	}
 	
-	return steps[n];
+	return ways[n];
 }
 
 /*
@@ -40,7 +40,7 @@ int climbStairs(int n)
 int main()
 {
 	int steps = 5;
-	int ways = climbStairs(steps)
+	int ways = climbStairs(steps);
 	
 	cout << ways;
 	
