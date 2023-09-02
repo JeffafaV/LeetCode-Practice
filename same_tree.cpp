62f67b3ad4f1d4f32ae5efe08ba99313ea0dd80a
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-09-01 21:52:27 -0700

    Update same_tree.cpp

--- same_tree.cpp
+++ same_tree.cpp
@@ -4,78 +4,124 @@
 
 using namespace std;
 
+// struct for a node in the binary tree
 struct TreeNode
 {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
+	int val; // value of the node
+	TreeNode *left; // left child node pointer
+	TreeNode *right; // right child node pointer
 	
+	// parameterized constructor
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// creates a binary tree and returns the root of the tree
 TreeNode *createBinaryTree()
 {
+	// create root node and initialize it to null
 	TreeNode *root = NULL;
+	
+	// create a queue variable to hold TreeNode pointers
+	// queue is necessary in order to create nodes in level order
 	queue<TreeNode *> q;
+	
+	// used to store yes or no responses from the user
 	string s;
+	
+	// display text and take user input
 	cout << "Insert root node? y for yes or n for no: ";
 	cin >> s;
 	
+	// user doesn't want to insert a root node
 	if (s == "n")
 	{
-		return NULL;
+		// return null since the tree is empty
+		return root;
 	}
 	
+	// used to store a value for a node
 	int val;
+	
+	// enter node value for root node
 	cout << "Enter root node value: ";
 	cin >> val;
 	
+	// create a new node in heap using the user's entered value and set it as the root
 	root = new TreeNode(val);
+	// push the root into the queue
 	q.push(root);
 	
+	// keep looping until the queue is empty because we 
+	// want to insert all the nodes into the tree and 
+	// before they reach the tree they go into the queue
 	while (q.size() != 0)
 	{
+		// used to create a new node
 		TreeNode *t = NULL;
+		// holds the first node in the queue
 		TreeNode *p = q.front();
+		// remove the first node from the queue
 		q.pop();
 		
+		// display text and take user input
 		cout << "Insert left child node for " << p->val << "? y for yes or n for no: ";
 		cin >> s;
 		
+		// user wants to insert a left child node
 		if (s == "y")
 		{
+			// enter node value for left child node
 			cout << "Enter left child node value: ";
 			cin >> val;
 			
+			// create a new node in heap using the user's entered value
 			t = new TreeNode(val);
+			// set the new node as the left child node of p
 			p->left = t;
+			// push the new node into the queue
 			q.push(t);
 		}
 		
+		// display text and take user input
 		cout << "Insert right child node for " << p->val << "? y for yes or n for no: ";
 		cin >> s;
 		
+		// user wants to insert a right child node
 		if (s == "y")
 		{
+			// enter node value for right child node
 			cout << "Enter right child node value: ";
 			cin >> val;
 			
+			// create a new node in heap using the user's entered value
 			t = new TreeNode(val);
+			// set the new node as the right child node of p
 			p->right = t;
+			// push the new node into the queue
 			q.push(t);
 		}
 	}
 	
+	// return the root node of the tree
 	return root;
 }
 
-// this a function
+// extra storage method
+// time complexity: O(n) because we only ever loop at most 2n times where 
+// n is the number of nodes
+// space complexity: O(n) because our containers will use a total of 4n space
+// in order to store all the node and null address values
 bool isSameTree(TreeNode *p, TreeNode *q)
 {
 	queue<TreeNode *> pQueue;
 	pQueue.push(p);
+	// used to store all the node and null address values of p's tree
 	vector<TreeNode *> pVector;
 	
+	// continue until pQueue is empty
+	// note that we also store null in the queue
+	// which will increase the number of loops to 2n
+	// and will give pVector a size of 2n
 	while (pQueue.size() != 0)
 	{
 		TreeNode *p1;
@@ -94,6 +140,10 @@ bool isSameTree(TreeNode *p, TreeNode *q)
 	qQueue.push(q);
 	vector<TreeNode *> qVector;
 	
+	// continue until qQueue is empty
+	// note that we also store null in the queue
+	// which will increase the number of loops to 2n
+	// and will give qVector a size of 2n
 	while (qQueue.size() != 0)
 	{
 		TreeNode *q1;
@@ -127,11 +177,22 @@ bool isSameTree(TreeNode *p, TreeNode *q)
 	return true;
 }
 
+/*
+	Given the roots of two binary trees p and q, write a function to check if 
+	they are the same or not. Two binary trees are considered the same if they 
+	are structurally identical, and the nodes have the same value.
+	
+	Constraints:
+	The number of nodes in both trees is in the range [0, 100].
+	-10^4 <= Node.val <= 10^4
+*/
 int main()
 {
+	// set the root node and create the first binary tree
 	cout << "First binary tree" << endl;
 	TreeNode *root1 = createBinaryTree();
 	
+	// set the root node and create the second binary tree
 	cout << endl << "Second binary tree" << endl;
 	TreeNode *root2 = createBinaryTree();
 	
