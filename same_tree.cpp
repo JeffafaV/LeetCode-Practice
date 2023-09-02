d178b1a8478a89c9b640675b7df86733266840b1
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2023-09-02 16:53:11 -0700

    Update same_tree.cpp

--- same_tree.cpp
+++ same_tree.cpp
@@ -107,13 +107,16 @@ TreeNode *createBinaryTree()
 }
 
 // extra storage method
-// time complexity: O(n) because we only ever loop at most 2n times where 
-// n is the number of nodes
+// time complexity: O(n) because we only ever loop at most 2n times in each loop and 
+// n is the number of nodes for both trees
 // space complexity: O(n) because our containers will use a total of 4n space
 // in order to store all the node and null address values
 bool isSameTree(TreeNode *p, TreeNode *q)
 {
+	// queue used to hold the first tree's TreeNode pointers
+	// used to help go through the tree in level order
 	queue<TreeNode *> pQueue;
+	// push p's root node into the queue
 	pQueue.push(p);
 	// used to store all the node and null address values of p's tree
 	vector<TreeNode *> pVector;
@@ -124,20 +127,31 @@ bool isSameTree(TreeNode *p, TreeNode *q)
 	// and will give pVector a size of 2n
 	while (pQueue.size() != 0)
 	{
+		// used to hold the first item in the queue
 		TreeNode *p1;
+		// set p1 to the first item in the queue which can be a node or null address
 		p1 = pQueue.front();
+		// push p1's value (node address or null) into the vector
 		pVector.push_back(p1);
+		// remove the first item from the queue
 		pQueue.pop();
 		
+		// p1 points to a node
 		if (p1 != NULL)
 		{
+			// push p1's left child to the queue
 			pQueue.push(p1->left);
+			// push p1's right child to the queue
 			pQueue.push(p1->right);
 		}
 	}
 	
+	// queue used to hold the second tree's TreeNode pointers
+	// used to help go through the tree in level order
 	queue<TreeNode *> qQueue;
+	// push q's root node into the queue
 	qQueue.push(q);
+	// used to store all the node and null address values of q's tree
 	vector<TreeNode *> qVector;
 	
 	// continue until qQueue is empty
@@ -146,34 +160,54 @@ bool isSameTree(TreeNode *p, TreeNode *q)
 	// and will give qVector a size of 2n
 	while (qQueue.size() != 0)
 	{
+		// used to hold the first item in the queue
 		TreeNode *q1;
+		// set q1 to the first item in the queue which can be a node or null address
 		q1 = qQueue.front();
+		// push q1's value (node address or null) into the vector
 		qVector.push_back(q1);
+		// remove the first item from the queue
 		qQueue.pop();
 		
+		// q1 points to a node
 		if (q1 != NULL)
 		{
+			// push q1's left child to the queue
 			qQueue.push(q1->left);
+			// push q1's right child to the queue
 			qQueue.push(q1->right);
 		}
 	}
 	
+	// loop through all pointer values in both trees using their vectors
+	// note that both trees have the same number of nodes (n nodes) 
+	// and we can use either pVector.size() or qVector.size()
 	for (int i = 0; i < pVector.size(); i++)
 	{
+		// current item in one tree is null and the 
+		// current item in the other tree is a node
 		if ((pVector[i] == NULL && qVector[i] != NULL) ||
 		(pVector[i] != NULL && qVector[i] == NULL))
 		{
+			// reaching here means that the trees don't have the 
+			// same structure so we return false
 			return false;
 		}
+		// current item in both trees are nodes
 		else if (pVector[i] != NULL && qVector[i] != NULL)
 		{
+			// current node values are not the same
 			if (pVector[i]->val != qVector[i]->val)
 			{
+				// reaching here means that the trees don't have the 
+				// same values in their nodes so we return false
 				return false;
 			}
 		}
 	}
 	
+	// reaching here means that the trees are structurally identical
+	// have the same node values
 	return true;
 }
 
